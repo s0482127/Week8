c6ebae9db8dea77fae68509422042affecdbb3b1
Alex Bertolasi <s0482127@student.rvc.edu>
2021-03-06 03:11:53 -0600

    Initial commit

---
+++ app/src/main/java/student/rvc/edu/week8/MainActivity2.kt
@@ -0,0 +1,46 @@
+package student.rvc.edu.week8
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val txtshow = findViewById<TextView>(R.id.txtShow)
+        //  val btnBack = findViewById<Button>(R.id.)
+
+
+
+        //loads intent string from MainActivity.kt
+
+        var strShow: String? = intent.getStringExtra("SendStuff")
+        txtshow.text = strShow
+
+
+        //Fire hidekeyboard when user taps outside any text object
+//Place below code right before last right bracket in function onCreate
+        findViewById<View>(android.R.id.content).setOnTouchListener { _, _ ->
+            hideKeyboard()
+            false
+        }
+
+    }
+
+    //function to hide keyboard goes right before the last right bracket of Class MainActivity
+//should auto import android.content.Context
+//should auto add import android.view.inputmethod.InputMethodManager
+    fun hideKeyboard() {
+        try {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        } catch (e: Exception) {
+            // TODO: handle exception
+        }
+
+    }
+}
